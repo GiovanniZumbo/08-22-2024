2b2eb7e003eea1c5b92aabfca0f1e0cee34a5489
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-22 15:42:24 +0200

    P&D data gathering

--- js-paliedispari/script-1.js
+++ js-paliedispari/script-1.js
@@ -7,17 +7,20 @@ Creare una funzione per capire se la parola inserita è palindroma */
 /*
 Prendo gli elementi dal DOM
 Leggi la parola da destra verso sinistra
-Confronta i due risultati
-Se sono uguali, la parola sarà palindroma
+Confronta i due risultati e logga
+
 */
 
 // # STAGING
 
 // # DATA GATHERING
 
-const userWord = prompt("Vuoi sapere se la tua parola è palindroma? Scrivila qui sotto.", 'Pottetto').trim();
+const userWord = prompt("Vuoi sapere se la tua parola è palindroma? Scrivila qui sotto.", 'ottetto').trim();
 let reversedWord = '';
 let message = '';
+
+
+// # PROCESSING
 
 //* Leggi la parola da destra verso sinistra
 
@@ -29,16 +32,14 @@ function mirrorWord(userWord) {
 
 console.log(mirrorWord(userWord));
 
-//Confronta i due risultati e logga
+//* Confronta i due risultati e logga
 
 message = (userWord === reversedWord) ? "La tua parola è un palindromo." : "La tua parola non è un palindromo.";
+
+// # OUTPUT
 
 console.log(message);
 
 
-// # EVENT HANDLING
-
-// # PROCESSING
 
 
-// # OUTPUT
---
+++ js-paliedispari/script-2.js
@@ -0,0 +1,30 @@
+// PLANNING
+
+
+/*
+Generare un numero random da 1 a 5
+Chiedere all'utente di scegliere tra pari e dispari
+Chiedere all'utente di scegliere un numero da 1 a 5
+Sommare i due numeri
+Stabilire se la somma è pari o dispari
+Stampare il risultato
+
+*/
+
+// # STAGING
+function getRandomNumber() {
+
+}
+
+// # DATA GATHERING
+
+// * Chiedere all'utente di scegliere tra pari e dispari
+const userChoice = prompt("Scegli tra pari o dispari", pari).trim();
+
+//* Chiedere all'utente di scegliere un numero da 1 a 5
+const userNumeber = parseInt(prompt("Dammi un numero da 1 a 5", 3));
+
+// # PROCESSING
+
+
+// # OUTPUT
