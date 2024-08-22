e71a15166e3afc2f260e4a87dbb7fa64cfccb36c
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-08-22 15:28:11 +0200

    Palindrome completed

--- js-paliedispari/script-1.js
+++ js-paliedispari/script-1.js
@@ -1,3 +1,44 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
+
+// PLANNING
+
+/*
+Prendo gli elementi dal DOM
+Leggi la parola da destra verso sinistra
+Confronta i due risultati
+Se sono uguali, la parola sarà palindroma
+*/
+
+// # STAGING
+
+// # DATA GATHERING
+
+const userWord = prompt("Vuoi sapere se la tua parola è palindroma? Scrivila qui sotto.", 'Pottetto').trim();
+let reversedWord = '';
+let message = '';
+
+//* Leggi la parola da destra verso sinistra
+
+function mirrorWord(userWord) {
+    // * Leggi la parola da destra verso sinistra
+    reversedWord = userWord.split("").reverse().join("");
+    return reversedWord;
+}
+
+console.log(mirrorWord(userWord));
+
+//Confronta i due risultati e logga
+
+message = (userWord === reversedWord) ? "La tua parola è un palindromo." : "La tua parola non è un palindromo.";
+
+console.log(message);
+
+
+// # EVENT HANDLING
+
+// # PROCESSING
+
+
+// # OUTPUT
